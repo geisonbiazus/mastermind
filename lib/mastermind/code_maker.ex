85a533d787ef9b80f9eeb5f65fbd3588ba99b141
Geison Biazus <geisonbiazus@gmail.com>
2018-12-12 13:24:09 -0200

    Return number of value matches

--- lib/mastermind/code_maker.ex
+++ lib/mastermind/code_maker.ex
@@ -1,13 +1,25 @@
 defmodule MasterMind.CodeMaker do
   def score(code, guess) do
-    [
-      position_matches(code, guess)
-    ]
+    p = position_matches(code, guess)
+    v = value_matches(code, guess)
+
+    [p, v - p]
   end
 
   defp position_matches(code, guess) do
     Stream.zip(code, guess)
     |> Stream.map(fn {a, b} -> a == b end)
+    |> count_true
+  end
+
+  defp value_matches(code, guess) do
+    guess
+    |> Stream.map(&Enum.member?(code, &1))
+    |> count_true
+  end
+
+  defp count_true(bool_list) do
+    bool_list
     |> Stream.filter(& &1)
     |> Enum.count()
   end
--- test/mastermind/code_maker_test.exs
+++ test/mastermind/code_maker_test.exs
@@ -3,23 +3,39 @@ defmodule MasterMind.CodeMakerTest do
 
   alias MasterMind.CodeMaker
 
-  test "score guess with no matches" do
-    assert CodeMaker.score([0, 0, 0, 0], [1, 1, 1, 1]) == [0]
+  describe "scoring position matches" do
+    test "score guess with no matches" do
+      assert CodeMaker.score([0, 0, 0, 0], [1, 1, 1, 1]) == [0, 0]
+    end
+
+    test "score guess with one position matches" do
+      assert CodeMaker.score([0, 0, 0, 0], [0, 1, 1, 1]) == [1, 0]
+    end
+
+    test "score guess with two position matches" do
+      assert CodeMaker.score([0, 0, 0, 0], [0, 1, 1, 0]) == [2, 0]
+      assert CodeMaker.score([0, 0, 0, 0], [1, 0, 1, 0]) == [2, 0]
+      assert CodeMaker.score([0, 0, 0, 0], [0, 1, 0, 1]) == [2, 0]
+    end
+
+    test "score guess with many position matches" do
+      assert CodeMaker.score([1, 1, 1, 1], [0, 1, 1, 1]) == [3, 0]
+      assert CodeMaker.score([0, 0, 0, 0], [0, 0, 0, 1]) == [3, 0]
+      assert CodeMaker.score([1, 2, 3, 4], [1, 2, 3, 4]) == [4, 0]
+    end
   end
 
-  test "score guess with one position matches" do
-    assert CodeMaker.score([0, 0, 0, 0], [0, 1, 1, 1]) == [1]
+  describe "scoring value matches" do
+    test "value matches" do
+      assert CodeMaker.score([1, 2, 3, 4], [2, 0, 0, 0]) == [0, 1]
+      assert CodeMaker.score([1, 2, 3, 4], [2, 3, 0, 0]) == [0, 2]
+      assert CodeMaker.score([1, 2, 3, 4], [2, 4, 1, 0]) == [0, 3]
+      assert CodeMaker.score([1, 2, 3, 4], [4, 3, 2, 1]) == [0, 4]
+      assert CodeMaker.score([1, 2, 3, 4], [2, 3, 4, 1]) == [0, 4]
+    end
   end
 
-  test "score guess with two position matches" do
-    assert CodeMaker.score([0, 0, 0, 0], [0, 1, 1, 0]) == [2]
-    assert CodeMaker.score([0, 0, 0, 0], [1, 0, 1, 0]) == [2]
-    assert CodeMaker.score([0, 0, 0, 0], [0, 1, 0, 1]) == [2]
-  end
-
-  test "score guess with many position matches" do
-    assert CodeMaker.score([1, 1, 1, 1], [0, 1, 1, 1]) == [3]
-    assert CodeMaker.score([0, 0, 0, 0], [0, 0, 0, 1]) == [3]
-    assert CodeMaker.score([1, 2, 3, 4], [1, 2, 3, 4]) == [4]
+  test "guesses with some position and some value matches" do
+    assert CodeMaker.score([1, 2, 3, 4], [1, 2, 4, 3]) == [2, 2]
   end
 end
