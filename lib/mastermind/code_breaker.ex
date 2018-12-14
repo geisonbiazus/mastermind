9996dd4b382ddb7baf016f6b34a158ecc430279b
Geison Biazus <geisonbiazus@gmail.com>
2018-12-14 09:05:33 -0200

    Add guess_to_number and number_to_guess

--- lib/mastermind/code_breaker.ex
+++ lib/mastermind/code_breaker.ex
@@ -1,5 +1,19 @@
 defmodule MasterMind.CodeBreaker do
   def break_code(past_guesses) do
-    {0, 0, 0, 0}
+    [0, 0, 0, 0]
+  end
+
+  def guess_to_number(guess) do
+    guess
+    |> Enum.reduce(&(6 * &2 + &1))
+  end
+
+  def number_to_guess(n) do
+    [
+      rem(div(n, 6 * 6 * 6), 6),
+      rem(div(n, 6 * 6), 6),
+      rem(div(n, 6), 6),
+      rem(n, 6)
+    ]
   end
 end
--- test/mastermind/code_breaker_test.exs
+++ test/mastermind/code_breaker_test.exs
@@ -4,6 +4,26 @@ defmodule MasterMind.CodeBreakerTest do
   alias MasterMind.CodeBreaker
 
   test "initial guess" do
-    assert CodeBreaker.break_code({}) == {0, 0, 0, 0}
+    assert CodeBreaker.break_code({}) == [0, 0, 0, 0]
+  end
+
+  test "guess_to_number" do
+    assert CodeBreaker.guess_to_number([0, 0, 0, 0]) == 0
+    assert CodeBreaker.guess_to_number([0, 0, 0, 1]) == 1
+    assert CodeBreaker.guess_to_number([0, 0, 1, 0]) == 6
+    assert CodeBreaker.guess_to_number([0, 0, 1, 1]) == 7
+    assert CodeBreaker.guess_to_number([0, 1, 1, 1]) == 43
+    assert CodeBreaker.guess_to_number([1, 1, 1, 1]) == 259
+    assert CodeBreaker.guess_to_number([5, 5, 5, 5]) == 6 * 6 * 6 * 6 - 1
+  end
+
+  test "number_to_guess" do
+    assert CodeBreaker.number_to_guess(0) == [0, 0, 0, 0]
+    assert CodeBreaker.number_to_guess(1) == [0, 0, 0, 1]
+    assert CodeBreaker.number_to_guess(6) == [0, 0, 1, 0]
+    assert CodeBreaker.number_to_guess(7) == [0, 0, 1, 1]
+    assert CodeBreaker.number_to_guess(43) == [0, 1, 1, 1]
+    assert CodeBreaker.number_to_guess(259) == [1, 1, 1, 1]
+    assert CodeBreaker.number_to_guess(6 * 6 * 6 * 6 - 1) == [5, 5, 5, 5]
   end
 end
