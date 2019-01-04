1563262a5a8492f54093f79cc05037c9405c68ad
Geison Biazus <geisonbiazus@gmail.com>
2019-01-04 14:00:48 -0200

    Add statistical tools

--- lib/mastermind/auto_play.ex
+++ lib/mastermind/auto_play.ex
@@ -15,6 +15,38 @@ defmodule MasterMind.AutoPlay do
       auto_play(code, tries + 1, [[guess, score] | past_scores], guess)
     end
   end
+
+  def expected_turns(n) do
+    scores = Enum.sort(Enum.map(1..n, fn _ -> play() end))
+
+    %{
+      mean: mean(scores),
+      sigma: sigma(scores),
+      min: hd(scores),
+      max: List.last(scores),
+      median: median(scores),
+      hist: hist(scores)
+    }
+  end
+
+  defp mean(x) do
+    Enum.sum(x) / Enum.count(x)
+  end
+
+  defp sigma(x) do
+    mn = mean(x)
+
+    v = Enum.sum(Stream.map(x, &:math.pow(&1 - mn, 2)))
+    :math.sqrt(v / (Enum.count(x) - 1))
+  end
+
+  defp median(x) do
+    Enum.at(x, div(Enum.count(x), 2))
+  end
+
+  defp hist(x) do
+    Enum.map(Enum.chunk_by(x, & &1), &Enum.sum/1)
+  end
 end
 
 defmodule MasterMind.CodeRandomizer do
